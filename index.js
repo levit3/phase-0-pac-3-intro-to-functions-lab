3376299fed37bb30789b83211d8fa6ad827d3bf6
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-03-13 13:20:15 +0300

    created the necessary functions

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+    return string.toUpperCase()
+}
+function whisper(string){
+    return string.toLowerCase()
+}
+function logShout(string) {
+    return console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+    return console.log(string.toLowerCase())
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    switch (string) {
+        case (string.toLowerCase()):
+            return ("I can\'t hear you!")
+        case (string.toUpperCase()):
+            return ("YES INDEED!")
+        case ("Let\'s have dinner together!"):
+            return ("I would love to!")
+    }
+}
